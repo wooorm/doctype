b8488f1634124e4799c74a8768fb81c24f21355e
Titus Wormer <tituswormer@gmail.com>
2019-05-11 09:46:14 +0200

    Refactor comment style

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var doctypes = require('./index.json')
 module.exports = getDoctype
 getDoctype.all = doctypes
 
-/* Gather all doctypes as shortcodes. */
+// Gather all doctypes as shortcodes.
 var shortcodes = gather()
 
-/* Ensure proper non-versioned types work: */
+// Ensure proper non-versioned types work:
 shortcodes.h = shortcodes.h5
 shortcodes.s = shortcodes['s1.1f']
 shortcodes.m = shortcodes.m2
 shortcodes.x = shortcodes['x1.1']
 
-/* Get a doctype from a name. */
+// Get a doctype from a name.
 function getDoctype(name) {
   var key = cleanDoctype(name)
   return shortcodes[key] || shortcodes['h' + key] || null
 }
 
-/* Clean and simplify a doctype name. */
+// Clean and simplify a doctype name.
 function cleanDoctype(name) {
   return String(name)
     .toLowerCase()
@@ -36,12 +36,12 @@ function cleanDoctype(name) {
     .replace(/\s+/g, '')
 }
 
-/* Get the first character of the second parameter. */
+// Get the first character of the second parameter.
 function character(_, value) {
   return value.charAt(0)
 }
 
-/* Clean all doctypes. */
+// Clean all doctypes.
 function gather() {
   var codes = {}
   var key
